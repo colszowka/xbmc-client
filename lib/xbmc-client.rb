1b98ff75b910cdead05cb1fbcd74f4960a45251a
Peter Fern <github@obfusc8.org>
2013-03-12 04:12:37 +1100

    Use a `SecureRandom.uuid` as the `id` on requests to handle concurrency

--- lib/xbmc-client.rb
+++ lib/xbmc-client.rb
@@ -21,7 +21,7 @@ class Xbmc
     # HTTParty::Response)
     def invoke_method(method, params={})
       response = post('/jsonrpc',
-                      :body => {"jsonrpc" => "2.0", "params" => params, "id" => "1", "method" => method}.to_json,
+                      :body => {"jsonrpc" => "2.0", "params" => params, "id" => SecureRandom.uuid, "method" => method}.to_json,
                       :headers => {'Content-Type' => 'application/json'})
       raise Xbmc::UnauthorizedError, "Could not authorize with XBMC. Did you set up your credentials for basic_auth using Xbmc.basic_auth 'user', 'pass'?" if response.response.class == Net::HTTPUnauthorized
       response
